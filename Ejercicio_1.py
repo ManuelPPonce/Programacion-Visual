6507f5da4be8a24b82003cbba6b639c1d592eefc
Manuel PP <manuel.pp2209@gmail.com>
2019-10-14 18:32:07 -0500

    Mensaje Personal

---
+++ Ejercicio_1.py
@@ -0,0 +1,25 @@
+# EJERCICIO DE PYTHON 1
+
+"""
+NOMBRE: Manuel Perez Ponce
+MATERIA: Programacion Visual
+"""
+
+# Mensaje personal:
+
+nombre = "Manuel"
+print("Hola", nombre, "como te encuentras el dia de hoy?")
+
+
+# Casos de nombre:
+
+nombre = "Josue"
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.title())
+
+
+# Numero favorito:
+
+num_fav = 22
+print("Mi numero favorito es:", num_fav)
