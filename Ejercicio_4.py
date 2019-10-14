79648b091b515806aff08aa5c106dbb690b4c6b8
Manuel PP <manuel.pp2209@gmail.com>
2019-10-14 18:40:25 -0500

    Ejercicio #4

---
+++ Ejercicio_4.py
@@ -0,0 +1,8 @@
+#Validar si eres mayor de edad (18) puedes votar
+
+edad = int(input('Ingresa tu edad : '))
+
+if edad < 18:
+	print ("No puedes votar , Eres menor de edad ")
+else:
+	print("Puedes votar")
